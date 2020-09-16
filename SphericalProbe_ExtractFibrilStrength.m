220d35581ab454f44dee7e3947852067c6a29c1e
jamieabooth <68619630+jamieabooth@users.noreply.github.com>
2020-09-16 14:38:51 -0700

    Add files via upload

--- SphericalProbe_ExtractFibrilStrength.m
+++ SphericalProbe_ExtractFibrilStrength.m
@@ -1,7 +1,7 @@
 % Fibrillar array discrete contact mechanics - spherical probe test 
 % Public code for Bettscheider et al. (2020)
 % Main code to extract fibril strength measurement
-% Jamie Booth 6-23-2020
+% Jamie Booth 9-7-2020
 
 clear all;
 
@@ -10,30 +10,29 @@ clear all;
 disp('Running...')
 
 % Dimensionless parameters
-h_ = h/(2*c);
-d_ = d/c;
-R_ = R/c;
+h_ = h/(2*c); % Fibril height
+d_ = d/c; % Fibril spacing
+R_ = R/c; % Probe radius
 
-% Standard simulation parameters
+% Initialize variables
 u_p = 0; % Starting prescribed dimensionless displacement
-du_p = 0.01; % Prescribed dimensionless displacement increment
-
-% Use adaptive step size in emax based on bisection method
-i = 1;
-e = Inf;
-emax = 0;
-Fmax = 0;
-demax = 0.01;
+du_p = 0.01; % Starting prescribed dimensionless displacement increment
+i = 1; % Initialize loop counter
+e = Inf; % Initialize error
+emax = 0; % Initialize 
+Fmax = 0; % Initialize detachment force
+demax = 0.01; % Starting strain increment
 
 while e > 0.005
     
+    % Increment fibril strain to detachent
     i = i + 1;
     emax = [emax emax(i-1)+demax];
 
-    % Schargott solution provides lower bound on critical contact radius
-    % Use 10% larger radius as starting point for array size
+    % Schargott solution provides lower bound on contact radius
+    % Use as starting point for array size
     a_crit = (2*max(emax(i))*h_*R_)^0.5;
-    arraysizeerr = 1;
+    arraysizeerr = 1; % Error indicating full contact prior to saturation of pull-off force
 
     % Process...
     % 1. Test array to full contact
@@ -46,47 +45,59 @@ while e > 0.005
 
         a_crit = 1.1*a_crit; % 10% increase in array size if full contact is made prior to attaining saturation
         
+        % Generate array positions
         switch hex_yn
             case 0 % Square
                 [x_,y_,N] = SphericalProbe_SquareArray(a_crit,d_);
             case 1 % Hex
                 [x_,y_,N] = SphericalProbe_HexArray(a_crit,d_); 
         end
-
+        
+        % Generate compliance matrix
         [c_fib,c_BL] = SphericalProbe_Compliance(x_,y_,h_,v);
-        fc_ = ones(N,1); % Uniform fibril strength
-
-        [~,~,~,~,F_data,~] = SphericalProbe_ForceDispAllFibrils(u_p,du_p,R_,h_,N,x_,y_,emax(i),v,c_fib,c_BL,fc_);
-        F_max1 = max(F_data);
-
+        % Prescribe uniform fibril strength
+        fc_ = ones(N,1);
+        % Perform model adhesion test with preload to full contact and output pull-off force
+        F_max1 = SphericalProbe_ForceDispAllFibrils(u_p,du_p,R_,h_,N,x_,y_,emax(i),v,c_fib,c_BL,fc_);
+        
+        % Use pull-off force as starting point for preload
         P_ = F_max1;
-        preloaderr = 1;
-
+        preloaderr = 1; % Error indicating saturation of pull-off force with preload not observed
+        
         while preloaderr == 1
 
             P_ = 1.05*P_; % 5% increase in preload if saturation not observed
-            [~,~,~,~,F_data,Nadata] = SphericalProbe_ForceDispPreload(u_p,du_p,R_,h_,P_,N,x_,y_,emax(i),v,c_fib,c_BL,fc_);
-            F_max2 = max(F_data);
-
+            
+            % Perform model adhesion test with preload to specified force and output pull-off force
+            [F_max2,Namax] = SphericalProbe_ForceDispPreload(u_p,du_p,R_,h_,P_,N,x_,y_,emax(i),v,c_fib,c_BL,fc_);
+            
+            % Test for saturation of pull-off force
             conv = abs(F_max2 - F_max1)/F_max2;
             if conv > 0.01
                 preloaderr = 1;
             else
                 preloaderr = 0;
             end
-
-            arraysizeerr = max(Nadata)/N;
+            
+            % Test for full contact of array
+            arraysizeerr = Namax/N;
 
         end
 
     end
     
+    % Model saturation pull-off force
     Fmax = [Fmax F_max2.*pi.*c.^2.*E.*emax(i)];
+    % Error between experimental and model saturation pull-off force
     e = abs((Fmaxexp-Fmax(i))/Fmaxexp);
+    % Increment fibril strain to detachment using adaptive step size based on secant method
     demax = (Fmaxexp-Fmax(i))*(emax(i)-emax(i-1))/(Fmax(i)-Fmax(i-1));
+    % Cap strain increment to avoid excessive computational cost
+    demax = sign(demax)*min([abs(demax) 0.25]);
     
 end
 
+% Output fibril strength parameters
 emaxexp = emax(i)
 fmaxexp = pi*c^2*E*emaxexp
 switch hex_yn
--- SphericalProbe_ForceDispAllFibrils.m
+++ SphericalProbe_ForceDispAllFibrils.m
@@ -3,118 +3,87 @@
 % Single test specified to contact all fibrils in array
 % Jamie Booth 6-23-2020
 
-function [u_pdata,u_fibdata,u_BLdata,f_data,F_data,Nadata] = SphericalProbe_ForceDispAllFibrils(u_p,du_p,R_,h_,N,x_,y_,emax,v,c_fib,c_BL,fc_)
+function F_max = SphericalProbe_ForceDispAllFibrils(u_p,du_p,R_,h_,N,x_,y_,emax,v,c_fib,c_BL,fc_)
 
+% Initialize arrays
 att_yn = false(N,1);
-f_= zeros(N,1);
-fib= transpose(1:N);
+fib= 1:N';
+F_max = [];
+Namax = [];
 
+% Constant system properties
 z_fibtip = (1./(emax*(1-v^2)))*2*h_*ones(N,1);
-
-count = 0; % Increment count
-
-u_pdata = [];
-u_fibdata = [];
-u_BLdata = [];
-f_data = [];
-F_data = [];
-Nadata = [];
-
 c_ = c_fib + c_BL;
 
-% Approach
+% Increment count
+count = 1; 
 
+% Approach
 while sum(att_yn) < N 
     
+    % Increment displacment
+    u_p = u_p - du_p;
     count = count + 1;
     
-    u_pdata(count) = u_p;
-    
+    % Determine relative position of surface and fibril tips
     u_ = u_p*ones(N,1) + (1./(emax*(1-v^2)))*(x_.^2./(2*R_)+y_.^2./(2*R_));
-    
     z_surf = (1./(emax*(1-v^2)))*2*h_*ones(N,1) + u_;
+    % Log attachment of fibrils
     att_yn(z_fibtip >= z_surf) = 1;
     
-    att_ind = 1; % Forces following while loop to run at least once
-    
-    while isempty(att_ind) == 0 % Runs reduction of c and recalculation of f_fmax until all fibrils are not critical
+    % Repeat until no further attachment occurs
+    att_ind = 1; % Force loop to run at least once
+    while isempty(att_ind) == 0
         
-        
+        % Calculate fibril forces
         fibred = fib(att_yn==1);
-        
-        u_red = u_(fibred);
-
-        c_red = c_(fibred,fibred);
-        k_red = eye(length(fibred))/c_red;
-
-        f_red = k_red*u_red;
-        
+        f_red = c_(fibred,fibred)\u_(fibred);
         f_ = zeros(N,1);
         f_(fibred) = f_red;
-        f_data(:,count) = f_;
-    
-        u_fibdata(:,count) = c_fib*f_;
-        u_BLdata(:,count) = c_BL*f_;
-        
-        z_fibtip = (1./(emax*(1-v^2)))*2*h_*ones(N,1) + u_fibdata(:,count) + u_BLdata(:,count);
-        
-        
+        % Calculate deformation
+        u_fib = c_fib*f_;
+        u_BL = c_BL*f_;
+        % Determine relative position of surface and fibril tips
+        z_fibtip = (1./(emax*(1-v^2)))*2*h_*ones(N,1) + u_fib + u_BL;
+        % Log attachment of fibrils
         att_ind = setdiff(fib(z_fibtip >= z_surf),fib(att_yn));
         att_yn(att_ind) = 1;
     
     end
-
-    F_ = sum(f_);
-    F_data = [F_data F_];
-    Nadata = [Nadata sum(att_yn)];
-
-    u_p = u_p - du_p;
     
+    F_ = sum(f_);
+    Na = sum(att_yn);
+    
+    F_max = max([F_max F_]);
+    Namax = max([Namax Na]);
+
 end
 
-
 % Retraction
-
 while sum(att_yn) > 0 
     
+    % Increment displacement
+    u_p = u_p + du_p;
     count = count + 1;
     
-    u_pdata(count) = u_p;
-    
+    % Determine fibril forces until no further detachements occur 
     u_ = u_p*ones(N,1) + (1./(emax*(1-v^2)))*(x_.^2./(2*R_)+y_.^2./(2*R_));
-    
-    det_ind = 1; % Forces following while loop to run at least once
-    
-    while isempty(det_ind) == 0 % Runs reduction of c and recalculation of f_fmax until all fibrils are not critical
-        
+    det_ind = 1; % Force loop to run at least once
+    while isempty(det_ind) == 0
         
         fibred = fib(att_yn==1);
-        fc_red = fc_(fibred);
-        
-        u_red = u_(fibred);
-
-        c_red = c_(fibred,fibred);
-        k_red = eye(length(fibred))/c_red;
-
-        f_red = k_red*u_red;
-        det_ind = fibred(f_red > fc_red);
-
+        f_red = c_(fibred,fibred)\u_(fibred);
+        det_ind = fibred(f_red > fc_(fibred));
         att_yn(det_ind) = 0;      
-        
     
     end
     
     f_ = zeros(N,1);
     f_(fibred) = f_red;
-    f_data(:,count) = f_;
-    
     F_ = sum(f_);
-    F_data = [F_data F_];
-    Nadata = [Nadata sum(att_yn)];
+    Na = sum(att_yn);
     
-    u_fibdata(:,count) = c_fib*f_;
-    u_BLdata(:,count) = c_BL*f_;
+    F_max = max([F_max F_]);
+    Namax = max([Namax Na]);
 
-    u_p = u_p + du_p;
-    
 end
